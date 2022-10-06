1a7ac72cd0614799a25bf6703ebab156854f48eb
Chebolu Naveen <naveen.chebolu123@gmail.com>
2022-10-06 17:19:52 +0530

    Added method-overriden

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-  
+    public static <sout> void main(String[] args) {
+        A ob = new B();
+        System.out.println(ob.meth());
+
+        B obj = new B() ;
+      System.out.println(obj.meth());
+
+
+    }
+}
+class A{
+  public String meth(){
+    return "Invoking method from class A";
+  }
+}
+class B extends A{
+  public String meth(){
+   return "Method is overridden in Extendend class B";
+  }
+
 }
